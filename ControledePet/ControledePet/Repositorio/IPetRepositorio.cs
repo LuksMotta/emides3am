2de829a119e2edc14f465219cb73375519c8fcd0
LuksMotta <lucas_motta2006@outlook.com>
2023-08-25 16:11:50 -0300

    CRUD

---
+++ ControledePet/ControledePet/Repositorio/IPetRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ControledePet.Models;
+
+namespace ControledePet.PetRepositorio
+{
+    public interface IPetRepositorio
+    {
+        PetModel ListarPorId(int id);
+        List<PetModel> BuscarTodos();
+        PetModel Adicionar(PetModel pet);
+        PetModel Atualizar(PetModel pet);
+        bool Apagar(int id);
+    }
+}
